cbe88ddd911b7651a8bea8173af51ba5042f6d25
XeContrast <1683481541@qq.com>
2025-09-12 09:39:44 +0800

    使用绝对布局

--- src/main/java/swing/SwingMain.java
+++ src/main/java/swing/SwingMain.java
@@ -4,28 +4,51 @@ import cpp.Connect;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 /**
  * @author XeContrast
+ * AICode 5%
  * 老子等你的CPP代码你人呢我操你妈的
  */
 public class SwingMain {
     public static String username = "";
     public static void startSwing() {
+        //Show GUI's Setting
         JFrame jFrame = new JFrame();
         jFrame.setLocation(570, 320);
         jFrame.setSize(570, 320);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setTitle("Minecraft Launcher");
-        Container container = jFrame.getContentPane();
-        JLabel jLabel = new JLabel("Username: " + username);
-        jLabel.setBounds(60, 10, 80, 30);
-        container.add(jLabel);
-        JButton jButton = new JButton("Start");
-        container.add(jButton, BorderLayout.SOUTH);
-        JButton setUsername = new JButton("SetUsername");
-        container.add(setUsername,BorderLayout.NORTH);
+        jFrame.setLayout(null);
         jFrame.setVisible(true);
+
+        Container container = jFrame.getContentPane();
+
+        //Show Text
+        JLabel jLabel = new JLabel("Username: " + username);
+        jLabel.setBounds(10, 10, 80, 30);
+        container.add(jLabel);
+
+        //Start Button
+        JButton jButton = new JButton("Start");
+        jButton.setBounds(jFrame.getWidth() - 180,jFrame.getHeight() - 100,120,40);
+        container.add(jButton);
+
+        //SetUsername Button
+        JButton setUsername = new JButton("SetUsername");
+        setUsername.setBounds(jFrame.getWidth() - 240,jFrame.getHeight() - 100,120,40);
+        container.add(setUsername);
+
+        //Update Start Button XYZ
+        jFrame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                jButton.setLocation(jFrame.getWidth() - 140,jFrame.getHeight() - 80);
+                setUsername.setLocation(jFrame.getWidth() - 280,jFrame.getHeight() - 80);
+            }
+        });
 
         setUsername.addActionListener(e -> {
             String input = JOptionPane.showInputDialog(null, "SetUsername");
