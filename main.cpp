a2d4c855937200baee2ddc726c27d289d41c1eab
zcygod <3208287369@qq.com>
2025-09-16 16:58:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void help(){
     cout << "          --dev by zcygod" << endl;
     cout << "1. ./{filename} -run {version} --username {name} 以{username}运行{version}版本的minecraft"<<endl;
     cout << "2. ./{filename} -download {version} 下载{version}版本的Minecraft及支持库"<<endl;
-    cout << "3. ./{filename} -download_jdk8 下载java8（默认第一次运行会执行）"<<endl;
+    // cout << "3. ./{filename} -download_jdk8 下载java8（默认第一次运行会执行）"<<endl;
     cout << "3. ./{filename} -help 获取帮助"<<endl;
     cout << "========================================================="<<endl;
 }
@@ -49,12 +49,15 @@ int main(int argc,char* argv[]){
         if((string)argv[i]=="-run"){
             launcher::version=argv[i+1];
             launcher::username=argv[i+3];
+            if(launcher::version>="1.16.5"){
+            	launcher::set_java_path(".\\jre17\\bin\\java.exe");
+			}
             // }else{
             //     cout << "错误：-run 后缺少 --username {name}"<<endl;
             //     cout << "按Ctrl+C"<<endl;
             //     while(1);  
             // }
-            launcher::launch(launcher::version);
+            launcher::launch();
         }
 
     }
